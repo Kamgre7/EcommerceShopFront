28328e7b5f6d0a8427f4659f32728fc3b9d1c9ae
kamgre7@gmail.com <kamgre7@gmail.com>
2022-08-22 19:04:28 +0200

    Added eslint - airbnb config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from 'react';
 import {
   ChakraProvider,
   Box,
@@ -8,8 +8,8 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -18,7 +18,9 @@ export const App = () => (
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
           <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            Edit
+            <Code fontSize="xl">src/App.tsx</Code>
+            and save to reload.
           </Text>
           <Link
             color="teal.500"
@@ -33,4 +35,4 @@ export const App = () => (
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
