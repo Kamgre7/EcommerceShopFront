1332fc74109de8ee9777a58726368235a400666b
kamgre7@gmail.com <kamgre7@gmail.com>
2022-08-27 11:19:39 +0200

    Added first routes to app, installed formik, and chakra icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,50 @@ import * as React from 'react';
 import {
   ChakraProvider,
   Box,
-  VStack,
   Grid,
-  theme,
+  theme, Flex,
 } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Header } from './components/Header/Header';
+import { LoginForm } from './components/Forms/LoginForm';
+import { NotFoundView } from './View/NotFoundView';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
+
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8} />
+      <Grid minH="10vh" p={3}>
+        <Flex
+          width="100%"
+          justify="space-between"
+        >
+          <Header />
+          <ColorModeSwitcher justifySelf="flex-end" alignSelf="center" />
+        </Flex>
       </Grid>
     </Box>
+
+    <Routes>
+      {/* <Route path="/product" element={<ProductView />} />
+      <Route path="/product/:id" element={<SingleProductDetails />} />
+      <Route path="/product/form" element={<AddProductForm />} />
+      <Route path="/category/" element={<CategoryView />} />
+      <Route path="/category/form" element={<AddCategoryForm />} />
+      <Route path="/ranking" element={<RankingView />} />
+      <Route path="/cart" element={<CartView />} />
+      <Route path="/removing-list" element={<RemovingListView />} /> */}
+      {/*
+      <Route path="/register" element={<RegisterForm />} />
+*/}
+      <Route path="/login" element={<LoginForm />} />
+      {/*     <Route
+        path="/"
+        element={
+          <Navigate replace to="/product" />
+          }
+      /> */}
+      <Route path="/*" element={<NotFoundView />} />
+    </Routes>
   </ChakraProvider>
 );
